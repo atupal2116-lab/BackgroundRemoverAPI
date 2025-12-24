b261bf98632fa1e71a967865a7260d35e5aebc9a
Abidin <atupal2116@gmail.com>
2025-12-24 15:54:04 +0300

    ilk yukleme

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import Response
+from rembg import remove
+from PIL import Image
+import io
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Background Remover API Calisiyor!"}
+
+@app.post("/remove-bg")
+async def remove_background(file: UploadFile = File(...)):
+    # 1. Gelen resim dosyasini oku
+    image_data = await file.read()
+    
+    # 2. Resmi Pillow (PIL) formatina cevir
+    input_image = Image.open(io.BytesIO(image_data))
+    
+    # 3. YAPAY ZEKA SIHRI: Arka plani kaldir
+    output_image = remove(input_image)
+    
+    # 4. Resmi tekrar byte formatina cevir (Geri gondermek icin)
+    img_byte_arr = io.BytesIO()
+    output_image.save(img_byte_arr, format='PNG')
+    img_byte_arr = img_byte_arr.getvalue()
+    
+    # 5. Sonucu PNG resmi olarak dondur
+    return Response(content=img_byte_arr, media_type="image/png")

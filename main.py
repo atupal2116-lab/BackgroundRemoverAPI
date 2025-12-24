3b827ccd75d95d8a2f57f394ecef852dd4f450f6
Abidin <atupal2116@gmail.com>
2025-12-24 17:46:50 +0300

    Lite modele gecildi

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import Response
-from rembg import remove
+from rembg import remove, new_session
 from PIL import Image
 import io
 
 app = FastAPI()
 
+# Kucuk modelin oturumunu baslatiyoruz (u2netp sadece 4MB'dir)
+model_name = "u2netp"
+session = new_session(model_name)
+
 @app.get("/")
 def home():
-    return {"message": "Background Remover API Calisiyor!"}
+    return {"message": "Background Remover API (Lite Mode) Calisiyor!"}
 
 @app.post("/remove-bg")
 async def remove_background(file: UploadFile = File(...)):
@@ -18,10 +22,10 @@ async def remove_background(file: UploadFile = File(...)):
     # 2. Resmi Pillow (PIL) formatina cevir
     input_image = Image.open(io.BytesIO(image_data))
     
-    # 3. YAPAY ZEKA SIHRI: Arka plani kaldir
-    output_image = remove(input_image)
+    # 3. YAPAY ZEKA SIHRI: Arka plani kaldir (Lite model ile)
+    output_image = remove(input_image, session=session)
     
-    # 4. Resmi tekrar byte formatina cevir (Geri gondermek icin)
+    # 4. Resmi tekrar byte formatina cevir
     img_byte_arr = io.BytesIO()
     output_image.save(img_byte_arr, format='PNG')
     img_byte_arr = img_byte_arr.getvalue()
